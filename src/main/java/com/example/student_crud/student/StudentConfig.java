eb8c4579ad4bb5bca26d7b220468b1f844e8986e
gworkineh <g.workineh@hotmail.com>
2021-09-04 11:46:23 -0400

    First Commit

---
+++ src/main/java/com/example/student_crud/student/StudentConfig.java
@@ -0,0 +1,36 @@
+package com.example.student_crud.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student Mariam = new Student(
+                    "Workineh",
+                    "workineh@yahoo.com",
+                    LocalDate.of(1980, Month.JUNE,11)
+            );
+            Student Meaza = new Student(
+                    "Meaza",
+                    "meaza@yahoo.com",
+                    LocalDate.of(2003, Month.JANUARY,12)
+            );
+            Student Dawit = new Student(
+                    "Dawit",
+                    "dawit@yahoo.com",
+                    LocalDate.of(1978, Month.JANUARY,12)
+            );
+            repository.saveAll(
+                    List.of(Mariam,Meaza,Dawit)
+            );
+        };
+    }
+}
---
+++ src/main/java/com/example/student_crud/student/StudentController.java
@@ -0,0 +1,25 @@
+package com.example.student_crud.student;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/student")
+public class StudentController {
+    private final StudentService studentAService;
+    @Autowired
+    public StudentController(StudentService studentService) {
+        this.studentAService = studentService;
+    }
+
+    @GetMapping
+    public List<Student> getStudents(){
+      return studentAService.getStudents();
+    }
+    @PostMapping
+    public void registerNewStudent(@RequestBody Student student){
+        studentAService.addNewStudent(student);
+    }
+}
